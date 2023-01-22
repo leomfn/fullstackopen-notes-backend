b69584c620ee36b17822409171963bb4b5316f60
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-23 00:17:30 +0100

    update example notes app

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 const Note = require('./models/note')
 // const mongoose = require('mongoose')
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
@@ -18,7 +19,6 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
-app.use(express.static('build'))
 
 let notes = [
   {
@@ -50,27 +50,18 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note
     .findById(request.params.id)
     .then(note => {
-      response.json(note)
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -91,10 +82,27 @@ app.post('/api/notes', (request, response) => {
     .then(savedNote => {
       response.json(savedNote)
     })
+})
 
-  // notes = notes.concat(note)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  // response.json(note)
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => response.json(updatedNote))
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -102,6 +110,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
