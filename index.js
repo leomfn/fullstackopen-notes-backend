9c93d03060e394d4dec805534bc927e6144364f9
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-23 14:52:06 +0100

    eslint fixes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
-// const mongoose = require('mongoose')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -19,26 +18,6 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -81,7 +60,7 @@ app.post('/api/notes', (request, response, next) => {
 app.delete('/api/notes/:id', (request, response, next) => {
   Note
     .findByIdAndRemove(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
